e2f25f816692c732879287692c63d7377b0b1b54
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2025-01-22 18:57:22 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to  /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
-})
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parseUpadatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpadatedAt)
+
+  expect(responseBody.dependecies.database.version).toEqual("16.6")
+
+  expect(responseBody.dependecies.database.max_connections).toEqual(100)
+
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1)
+});
+
+test.only("Teste de SQL Injection", async () => {
+  await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+
+});
+

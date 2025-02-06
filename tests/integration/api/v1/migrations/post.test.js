98d967817d62a2f6c01e8b5544e067f98ea83d99
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2025-02-06 19:51:29 +0000

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,13 +1,15 @@
 import database from "infra/database.js";
+import orchestrator from "../orchestrator";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("DROP schema public CASCADE; CREATE schema public");
-}
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {method: 'POST'});
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
   expect(response1.status).toBe(201);
 
   const response1Body = await response1.json();
@@ -15,15 +17,14 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
 
-
   // Segundo POST
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {method: 'POST'});
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
   expect(response2.status).toBe(200);
 
   const response2Body = await response2.json();
 
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
-
-
-}); 
+});

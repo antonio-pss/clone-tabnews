9f075fa0c8916011804b345867296e07f60f347a
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2025-02-06 19:51:29 +0000

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,9 @@
+import orchestrator from "../orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
 test("GET to  /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
@@ -5,12 +11,11 @@ test("GET to  /api/v1/status should return 200", async () => {
   const responseBody = await response.json();
 
   const parseUpadatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpadatedAt)
+  expect(responseBody.updated_at).toEqual(parseUpadatedAt);
 
-  expect(responseBody.dependecies.database.version).toEqual("16.6")
+  expect(responseBody.dependecies.database.version).toEqual("16.6");
 
-  expect(responseBody.dependecies.database.max_connections).toEqual(100)
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
 
-  expect(responseBody.dependecies.database.opened_connections).toEqual(1)
-}); 
-
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
+});
